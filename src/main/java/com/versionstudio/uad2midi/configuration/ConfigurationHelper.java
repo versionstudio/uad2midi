9ff1be25c32271d4bff52f0008b5394cdb36ce0c
Erik Nilsson <software@versionstudio.com>
2020-11-09 21:53:10 +0100

    Initial commit

--- src/main/java/com/versionstudio/uad2midi/configuration/ConfigurationHelper.java
+++ src/main/java/com/versionstudio/uad2midi/configuration/ConfigurationHelper.java
@@ -10,17 +10,17 @@ import org.apache.logging.log4j.Logger;
 
 public class ConfigurationHelper {
 	private static final Logger logger = LogManager.getLogger(ConfigurationHelper.class);
-	
+
 	private final Properties properties;
 
 	public ConfigurationHelper() {
 		this.properties = new Properties();
 	}
-	
+
 	/**
 	 * Load configuration data from a resource bundle.
 	 * @param baseName the base name of the resource bundle
-	 */	
+	 */
 	public void load(String baseName) {
 		try {
 			ResourceBundle rb = ResourceBundle.getBundle(baseName);
@@ -33,51 +33,51 @@ public class ConfigurationHelper {
 			logger.info("Could not find any configuration resource bundle with base name: {}",baseName);
 		}
 	}
-	
-    /**
-     * Get a boolean property value.
-     * @param key the property key to find
-     * @return the property value
-     */
+
+	/**
+	 * Get a boolean property value.
+	 * @param key the property key to find
+	 * @return the property value
+	 */
 	public boolean getBooleanValue(String key) {
 		String value = getStringValue(key);
 		return Boolean.parseBoolean(value);
-    }
-    
-    /**
-     * Get a boolean property value.
-     * @param key the property key to find
-     * @param defaultValue the default value to return if no value was found
-     * @return the property value
-     */
+	}
+
+	/**
+	 * Get a boolean property value.
+	 * @param key the property key to find
+	 * @param defaultValue the default value to return if no value was found
+	 * @return the property value
+	 */
 	public boolean getBooleanValue(String key,boolean defaultValue) {
 		if ( this.properties.containsKey(key) ) {
 			return getBooleanValue(key);
 		} else {
 			return defaultValue;
 		}
-    }
-    
-    /**
-     * Get an int property value.
-     * @param key the property key to find
-     * @return the property value
-     */
+	}
+
+	/**
+	 * Get an int property value.
+	 * @param key the property key to find
+	 * @return the property value
+	 */
 	public int getIntValue(String key) {
 		String value = getStringValue(key);
-    	try {
-    		return Integer.parseInt(value);
-    	} catch(NumberFormatException e) {
-    		return -1;
-    	}
+		try {
+			return Integer.parseInt(value);
+		} catch(NumberFormatException e) {
+			return -1;
+		}
 	}
-    
-    /**
-     * Get a int property value.
-     * @param key the property key to find
-     * @param defaultValue the default value to return if no value was found
-     * @return the property value
-     */
+
+	/**
+	 * Get a int property value.
+	 * @param key the property key to find
+	 * @param defaultValue the default value to return if no value was found
+	 * @return the property value
+	 */
 	public int getIntValue(String key,int defaultValue) {
 		if ( this.properties.containsKey(key) ) {
 			return getIntValue(key);
@@ -85,34 +85,34 @@ public class ConfigurationHelper {
 			return defaultValue;
 		}
 	}
-    
-    /**
-     * Get a string property value.
-     * @param key the property key to find
-     * @return the property value
-     */
-    public String getStringValue(String key) {
-    	Object value = this.properties.getProperty(key);
-    	if ( value instanceof String ) {
-    		return (String)value;
-    	} else {
-    		return "";
-    	}
-    }
-    
-    /**
-     * Get a string property value.
-     * @param key the property key to find
-     * @param defaultValue the default value to return if no value was found
-     * @return the property value
-     */
-    public String getStringValue(String key,String defaultValue) {
-    	if ( this.properties.containsKey(key) ) {
-    		return getStringValue(key);
-    	} else {
-    		return defaultValue;
-    	}
-    }
+
+	/**
+	 * Get a string property value.
+	 * @param key the property key to find
+	 * @return the property value
+	 */
+	public String getStringValue(String key) {
+		Object value = this.properties.getProperty(key);
+		if ( value instanceof String ) {
+			return (String)value;
+		} else {
+			return "";
+		}
+	}
+
+	/**
+	 * Get a string property value.
+	 * @param key the property key to find
+	 * @param defaultValue the default value to return if no value was found
+	 * @return the property value
+	 */
+	public String getStringValue(String key,String defaultValue) {
+		if ( this.properties.containsKey(key) ) {
+			return getStringValue(key);
+		} else {
+			return defaultValue;
+		}
+	}
 
 	/**
 	 * Gets the property keys that start with the given prefix.
